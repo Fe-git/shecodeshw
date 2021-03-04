2d2a0d4fdfae59e21932f8edbaa93275038daffd
Fe-git <mjgemade@yahoo.com>
2021-03-04 20:59:57 +0000

    hard work
    we can now switch from celsius to fahrenheit and vice versa

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 function displayTemperature(response) {
-  console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let conditionElement = document.querySelector("#condition")
 let windElement = document.querySelector("#wind");
 let iconElement = document.querySelector("#icon");
 
-temperatureElement.innerHTML =Math.round (response.data.main.temp);
+celciusTemperature = Math.round (response.data.main.temp);
+temperatureElement.innerHTML =Math.round (celciusTemperature);
 cityElement.innerHTML = response.data.name;
 conditionElement.innerHTML = response.data.weather[0].description;
 windElement.innerHTML = Math.round (response.data.wind.speed);
 iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
+
+function check(city) {
 let apiKey = "d02508008068065146939cb2f3fbdbe7";
-let city = "Abuja";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-
 axios.get(apiUrl).then(displayTemperature);
+}  
 
 function searching(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-text-input");
+  check(searchInput.value);
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${searchInput.value}`;
 }
+
+
 let form = document.querySelector("#special");
 form.addEventListener("submit", searching);
 
@@ -45,4 +48,30 @@ let day = days[now.getDay()];
 let hours = now.getHours();
 let minutes = now.getMinutes();
 
-h5.innerHTML = ` ${date} ${day} ${hours}:${minutes}`;
+h5.innerHTML = `${date} ${day} ${hours}:${minutes}`;
+
+function displayFahrenheitElement(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celciusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+
+  let fahrenheitElement = (celciusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round (fahrenheitElement);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click",  displayFahrenheitElement);
+
+function displayCelciusTemperature(event) {
+  event.preventDefault();
+  celciusLink.classList.add("active");
+ fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML =  Math.round (celciusTemperature);
+}
+let celsiusTemperature = null;
+
+let celciusLink = document.querySelector("#celcius");
+celciusLink.addEventListener("click",  displayCelciusTemperature);
+
+check("Abuja");
